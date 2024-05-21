d9fcb8519d49a3c9e1d1b631160c5868cb953265
DESKTOP-6LDNJGR\54113 <ailenaguino@gmail.com>
2024-05-21 00:36:02 -0300

    Import fix to AddMedicationViewModel

--- app/src/main/java/com/losrobotines/nuralign/feature_medication/presentation/screens/AddMedicationViewModel.kt
+++ app/src/main/java/com/losrobotines/nuralign/feature_medication/presentation/screens/AddMedicationViewModel.kt
@@ -1,7 +1,7 @@
 package com.losrobotines.nuralign.feature_medication.presentation.screens
 
 import androidx.lifecycle.ViewModel
-import com.losrobotines.nuralign.feature_login.domain.AuthRepository
+import com.losrobotines.nuralign.feature_login.domain.providers.AuthRepository
 import javax.inject.Inject
 
 class AddMedicationViewModel @Inject constructor(private val repository: AuthRepository) :
