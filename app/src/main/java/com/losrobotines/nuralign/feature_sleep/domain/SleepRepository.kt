8f314bc545062bc4e40697a730649704d76818a1
Mateo <mateodinu02@gmail.com>
2024-05-28 23:46:58 -0300

    new select Reloj

--- app/src/main/java/com/losrobotines/nuralign/feature_sleep/domain/SleepRepository.kt
+++ app/src/main/java/com/losrobotines/nuralign/feature_sleep/domain/SleepRepository.kt
@@ -5,4 +5,6 @@ import com.losrobotines.nuralign.feature_sleep.domain.models.SleepInfo
 
 interface SleepRepository {
     suspend fun saveSleepData(sleepInfo: SleepInfo)
+
+    suspend fun getSleepData(patientId: Int): SleepInfo?
 }
--- app/src/main/java/com/losrobotines/nuralign/feature_sleep/presentation/screens/SleepViewModel.kt
+++ app/src/main/java/com/losrobotines/nuralign/feature_sleep/presentation/screens/SleepViewModel.kt
@@ -1,5 +1,8 @@
 package com.losrobotines.nuralign.feature_sleep.presentation.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -16,6 +19,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import javax.inject.Inject
 
+@RequiresApi(Build.VERSION_CODES.O)
 @HiltViewModel
 class SleepViewModel @Inject constructor(
     private val sleepRepository: SleepRepository,
@@ -23,33 +27,141 @@ class SleepViewModel @Inject constructor(
 ) :
     ViewModel() {
 
+    private val _isSaved = MutableLiveData(false)
+    val isSaved = _isSaved
+
     private val _sliderPosition = MutableLiveData(0F)
     var sliderPosition: LiveData<Float> = _sliderPosition
+
+    private val _sleepHours = mutableIntStateOf(0)
+    val sleepHours = _sleepHours
+
+    private val _negativeThoughts = MutableLiveData(false)
+    var negativeThoughts = _negativeThoughts
+
+    private val _anxiousBeforeSleep = MutableLiveData(false)
+    var anxiousBeforeSleep = _anxiousBeforeSleep
+
+    private val _sleptThroughNight = MutableLiveData(false)
+    var sleptThroughNight = _sleptThroughNight
+
+    private val _additionalNotes = MutableLiveData("")
+    val additionalNotes = _additionalNotes
+
+    private val _bedTime = MutableLiveData("")
+    val bedTime = _bedTime
+
+    fun setIsSaved(value: Boolean) {
+        _isSaved.value = value
+    }
+
+    fun setSleepTime(time: String) {
+        _bedTime.value = time
+    }
+
+    fun setAdditionalNotes(notes: String) {
+        _additionalNotes.value = notes
+    }
+
+    fun setNegativeThoughts(value: Boolean) {
+        _negativeThoughts.value = value
+    }
+
+    fun setAnxiousBeforeSleep(value: Boolean) {
+        _anxiousBeforeSleep.value = value
+    }
+
+    fun setSleptThroughNight(value: Boolean) {
+        _sleptThroughNight.value = value
+    }
+
+    fun setSleepTime(time: Int) {
+        _sleepHours.intValue = time
+    }
 
     fun onSliderChanged(sliderValue: Float) {
         _sliderPosition.value = sliderValue
     }
 
     fun saveData() {
-
-        if(currentUserExists()) {
+        if (currentUserExists()) {
             viewModelScope.launch {
-                sleepRepository.saveSleepData(
-                    SleepInfo(
-                        getPatentId(),
-                        getDate(),
-                        _sliderPosition.value!!.toInt().toShort(),
-                        1,
-                        "N",
-                        "N",
-                        "N",
-                        ""
+                _bedTime.value?.let {
+                    _additionalNotes.value?.let { aditionalNote ->
+                        SleepInfo(
+                            getPatentId(),
+                            getDate(),
+                            _sleepHours.intValue.toShort(),
+                           //  it.replace(":", ""),
+                            removeColonFromTime(it),
+                            //"1245",
+                            _negativeThoughts.value.toString()[0].uppercase(),
+                            _anxiousBeforeSleep.value.toString()[0].uppercase(),
+                            _sleptThroughNight.value.toString()[0].uppercase(),
+                            aditionalNote
+                        )
+                    }
+                }?.let {
+                    sleepRepository.saveSleepData(
+                        it
                     )
-                )
+                }
             }
         }
     }
+    private fun addColonToTime(time: String): String {
+        return time.chunked(2).joinToString(":")
+    }
+    private fun removeColonFromTime(time: String): String {
+        return time.replace(":", "")
+    }
 
+
+
+    /*
+        init {
+            loadMoodTrackerInfoToDatabase()
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun loadMoodTrackerInfoToDatabase() {
+            viewModelScope.launch {
+                try {
+                    if (currentUserExists()) {
+                        val patientId = getPatentId()
+                        val date = getDate()
+                        val info = sleepRepository.getSleepData(patientId.toInt())
+                        if (info != null) {
+                            sleepHours.intValue = info.sleepHours.toInt()
+                            bedTime.value = info.bedTime.toString()
+                            additionalNotes.value = info.sleepNotes
+                            if (info.negativeThoughtsFlag == "F"){
+                                setNegativeThoughts(false)
+                            } else {
+                                setNegativeThoughts(true)
+                            }
+                            if (info.anxiousFlag == "F"){
+                                setAnxiousBeforeSleep(false)
+                            } else {
+                                setAnxiousBeforeSleep(true)
+                            }
+                            if(info.sleepStraightFlag=="F"){
+                                setSleptThroughNight(false)
+                            }else{
+                                setSleptThroughNight(true)
+                            }
+                        } else {
+                            isSaved.value = false
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    isSaved.value = false
+                }
+            }
+        }
+
+     */
 
 
     private fun getDate(): String {
