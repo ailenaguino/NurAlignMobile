25006afb832e6e1deeb9a9eb699a485d011a9e43
lucianodelaiglesia <66572709+lucianodelaiglesia@users.noreply.github.com>
2024-06-29 11:04:24 -0300

    Test that fails

--- app/src/test/java/com/losrobotines/nuralign/feature_therapy/domain/usecases/therapists/EditExistingTherapistInListUseCaseTest.kt
+++ app/src/test/java/com/losrobotines/nuralign/feature_therapy/domain/usecases/therapists/EditExistingTherapistInListUseCaseTest.kt
@@ -8,6 +8,7 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
